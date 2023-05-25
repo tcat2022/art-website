67b6a71d98bb56bcec1db8b115d428454eeab980
tommy <tcatty7@gmail.com>
2023-05-24 22:18:38 -0400

    good

---
+++ script.js
@@ -0,0 +1,23 @@
+var el = document.documentElement;
+
+   
+
+function fullScreen(){
+
+      if(el.requestFullscreen){
+        el.requestFullscreen()  
+    }
+}
+
+(function(){
+    function scrollH(e){
+        e.preventDefault();
+        e = window.event || e;
+        let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+   document.getElementById("main").scrollLeft -= (delta * 100);
+    }
+    if( document.getElementById("main").addEventListener){
+        document.getElementById("main").addEventListener('mousewheel', scrollH,false);
+        document.getElementById("main").addEventListener('DOMMouseScroll', scrollH,false);
+    }
+})();
